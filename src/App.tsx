ed140f7604d366a585653a6a867d8d0fbc757b45
TonyBNK <borisenk-anton@yandex.ru>
2021-12-18 17:30:13 +0300

    Created project with hard code.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import c from './App.module.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [messages, setMessages] = useState([
+        {
+            id: "gsdrtarq",
+            message: "Hey Beav",
+            user: {id: "jhgfhj", name: "Butthead"}
+        },
+        {id: "kjhgjk", message: "Sup dude", user: {id: "hjfg", name: "Beavis"}}
+    ]);
+
+    return (
+        <div className={c.app}>
+            <div className={c.screen}>
+                {
+                    messages.map(message =>
+                        <div key={message.id}>
+                            <span>
+                                <b>{message.user.name}: </b>
+                            </span>
+                            <span>
+                                {message.message}
+                            </span>
+                        </div>)
+                }
+            </div>
+            <div className={c.panel}>
+                <textarea>
+
+                </textarea>
+                <button>
+                    Send
+                </button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
